105791e1527d5bdd4bac2967c37feba68834ef27
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-03-17 21:57:37 +0500

    chore: Update homepage hero section content and styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
     <div className="page">
-      <section className="header">
+      <section className="hero w-full my-5">
         <div className="container mx-auto">
-          
+          <div className="hero-area">
+
+            <h1 className="my-2">Welcome to my website!</h1>
+            <p className="my-1">Feel free to explore and learn more about me.</p>
+            <p className="mt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi fuga excepturi sapiente, sit aliquid explicabo labore voluptate ea corrupti illo! Quibusdam quod similique distinctio labore deserunt, quae minima obcaecati repellat. Aut dicta eius ullam, recusandae distinctio magnam tenetur pariatur sit animi! Repellat nemo voluptatibus dolor saepe maxime ad veniam hic vero modi aperiam, optio amet ratione accusamus quod officia, voluptatum ea vitae molestias dolorem. Officia, aperiam. Blanditiis nulla quasi earum voluptatum assumenda nobis, quis incidunt unde amet id voluptas provident aperiam necessitatibus totam minus odio vel labore deserunt! Ab ullam voluptates quas ad quasi optio quam similique. Commodi aspernatur distinctio iusto esse omnis sapiente cupiditate aliquid id iste dolorem debitis illo impedit deleniti ut voluptatem velit fugit fugiat mollitia quo non, pariatur, quasi repellendus odit perspiciatis. Autem veritatis corrupti animi quia qui numquam eius. Repudiandae cum, saepe placeat rerum blanditiis nisi itaque, molestias explicabo eveniet aliquam harum eligendi numquam id sequi deleniti tempore dolorem at quis fugiat totam corporis, maiores earum dolore. Ducimus incidunt vel id nesciunt, doloribus dignissimos expedita animi aliquid non molestias? Placeat quas in ipsa sunt omnis corrupti numquam eligendi rem! Provident, porro! Veritatis fuga dolor quidem in quaerat, at omnis quasi accusantium. Adipisci repellat quo debitis exercitationem voluptate beatae? Quidem hic iure molestias obcaecati fuga doloribus ratione vel incidunt aliquid neque sint, quos necessitatibus aliquam ducimus ipsum autem quia maxime. Ratione deserunt cumque ipsa. Officia magnam odit incidunt saepe blanditiis. Quos est maxime illum delectus, explicabo ipsam vel saepe autem, magnam ut fugiat esse cumque! Porro, ut. Fuga minima eos nulla omnis rerum, ullam voluptates architecto dolore corporis, beatae enim recusandae consectetur alias id velit. Iusto nemo suscipit id officia laboriosam incidunt alias voluptas corrupti dolores aut soluta sunt velit, maxime doloremque vitae. Doloribus, corporis. Tempore numquam quaerat eveniet vel ex, quae laudantium pariatur amet officiis!</p>
+          </div>
         </div>
       </section>
     </div>
