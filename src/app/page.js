e75bc8ef39fb0b6bffda5c6b21664d44415638f1
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-03-20 10:33:27 +0500

    chore: Update input focus styles for better accessibility and consistency

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
                     { icon: <FaInstagram />, text: "@mhgkhan", link: "/", color: "text-pink-500" },
                     { icon: <FaFacebook />, text: "@mhghazna", link: "/", color: "text-blue-500" },
                     { icon: <FaLinkedin />, text: "@mhghazna", link: "/", color: "text-blue-500" },
-                    { icon: <FaGithub />, text: "@mhgkhan", link: "/", color: "text-white" },
+                    { icon: <FaGithub />, text: "@mhgkhan", link: "/", color: "dar:text-white text-black" },
                     { icon: <FaWhatsapp />, text: "@mhghazna", link: "/", color: "text-green-600" },
                   ]).map((ele, ind) => {
                     return <div key={ind} className="saccount-block flex items-center justify-center gap-2 rounded-md  my-2 p-2 w-[190px] md:mx-0 mx-auto border border-1  dark:border-white border-gray-900">
@@ -56,14 +56,14 @@ export default function Home() {
               <p className="dark:text-gray-200 text-gray-600">Send your query directly from here. NOTE: you have only three message attempts</p>
               <form className="w-full p-2 mt-5">
                 <div className="first-row flex items-center justify-center gap-3 my-2 md:flex-row flex-col ">
-                  <input type="text" name="name" placeholder="Full name" className="dark:text-white text-black font-bold focus:bg-gray-700 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
-                  <input type="email" name="email" placeholder="Email" className="dark:text-white text-black font-bold focus:bg-gray-700 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+                  <input type="text" name="name" placeholder="Full name" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+                  <input type="email" name="email" placeholder="Email" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
                 </div>
                 <div className="first-row flex items-center justify-center gap-3 my-2 md:flex-row flex-col ">
-                  <input type="text" name="phone" placeholder="Phone" className="dark:text-white text-black font-bold focus:bg-gray-700 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
-                  <input type="text" name="Subject" placeholder="Subject" className="dark:text-white text-black font-bold focus:bg-gray-700 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+                  <input type="text" name="phone" placeholder="Phone" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+                  <input type="text" name="Subject" placeholder="Subject" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 md:w-[50%] w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
                 </div>
-                <textarea name="message" placeholder="Type message" rows={5}  className="font-bold w-full focus:bg-gray-700 rounded-md p-2 outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit"></textarea>
+                <textarea name="message" placeholder="Type message" rows={5}  className="font-bold w-full dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit"></textarea>
                 <button className="submit w-auto p-3 border-1 border border-gray-500 text-white hover:bg-pink-600 bg-pink-700 text-center my-2 flex items-center justify-center gap-2 rounded-md active:border-2 active:border-dotted"><span><FaPaperPlane /></span> Send</button>
               </form>
             </div>
