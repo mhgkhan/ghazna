2adb02c096e9abd4319b37449d9dd2e852b2a399
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-03-27 21:16:02 +0500

    Add Links to services blocks

--- src/components/ui/Services.jsx
+++ src/components/ui/Services.jsx
@@ -9,12 +9,12 @@ import Aos from 'aos';
 const Services = () => {
 
     const [leftServices, setLeftServices] = useState([
-        { title: "Web Development", link: "/" },
-        { title: "Data Entry", link: "/" },
+        { title: "Web Development", link: "/plans" },
+        { title: "Data Entry", link: "/plans/dataentry" },
     ]);
     const [rightServices, setRightServices] = useState([
-        { title: "Graphic Designing", link: "/" },
-        { title: "Printing Studio" },
+        { title: "Graphic Designing", link: "/plans/graphicdesigning" },
+        { title: "Printing Studio" , link:"/plans/printings"},
     ]);
 
     // const [services, setServices] = useState([
@@ -63,7 +63,7 @@ const Services = () => {
                             rightServices.map((ele, ind) => {
                                 return <div data-aos="fade-left" key={ind} className={`w-full h-[100px] md:mt-[${100 * ind}px] right-item-services dark:bg-white bg-gray-200  p-2 flex items-center justify-center flex-col gap-2`}>
                                     <h3 className="lg:text-3xl md:text-2xl text-center font-bold text-pink-600">{ele.title} </h3>
-                                    <Link href="#contact" className="mx-auto  btn md:px-4 px-3 py-3 rounded-md text-center hover:underline dark:bg-white dark:text-black bg-black text-white active:border active:border-2 dark:active:border-black border-white active:border-dotted">View Pricing</Link>
+                                    <Link href={ele.link} className="mx-auto  btn md:px-4 px-3 py-3 rounded-md text-center hover:underline dark:bg-white dark:text-black bg-black text-white active:border active:border-2 dark:active:border-black border-white active:border-dotted">View Pricing</Link>
                                 </div>
                             })
                         }
