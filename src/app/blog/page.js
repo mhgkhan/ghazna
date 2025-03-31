10445e63072aa29887f58ffa5008a54667a85e92
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-03-31 10:40:29 +0500

    Add Blog Cards to Container Blog Page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,15 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { FaComment, FaEye, FaHeart, FaMehRollingEyes } from 'react-icons/fa'
 
 const page = () => {
   return (
     <article className="min-h-screen">
-       <section className="w-full">
+      <section role="heading" className="w-full">
         <div className="container mx-auto">
-            <h1 className="text-4xl font-bold">Blog</h1>
-            <p className="my-2 text-gray-700 dark:text-gray-200">Explore the latest articles and tutorials on modern web development technologies.</p>  
-
+          <h1 className="md:p-auto p-5 text-4xl font-bold">Blog</h1>
+          <p className="md:p-auto p-5 my-2 text-gray-700 dark:text-gray-200">Explore the latest articles and tutorials on modern web development technologies.</p>
         </div>
-       </section>
+      </section>
+
+      <section className="blogs-container container mx-auto my-5 px-3 flex items-center justify-center gap-5 flex-wrap">
+
+        {
+          Array.from([1, 2, 3, 4, 5]).map((ele, ind) => {
+            return <div key={ind} className="blog-card md:w-[300px] w-[98%]  my-2 rounded-md shadow-md shadow-gray-900 p-1 md:h-[300px] h-auto border border-1 dark:border-gray-400 border-gray-600 ">
+              <Link href="https://mhgkhanp.netlify.app">
+                <div className="Image-area w-full h-[180px] border border-1 border-gray-600 dark:border-gray-400 rounded-md">
+                  <Image src="/images/services/fullstack.png" width={"350"} height={200} alt='Blog-Slug-picture' className='w-full h-full object-cover object-center rounded-md' ></Image>
+                </div>
+                <div className="content-area px-2 py-2">
+                  <h3 className='text-xl font-bold'> How to create a software without coding/AI How?</h3>
+                </div>
+                <div className='blog-card-footer px-3 w-full flex items-center justify-between py-1 pt-2 gap-7 border-1 border-t border-gray-500'>
+                  <div className="flex dark:text-gray-300 text-gray-600 items-center justify-center gap-2">
+                    <span className="text-2xl"><FaEye /></span> <span className='text-sm'>300+ Views </span>
+                  </div>
+                  <div className="flex dark:text-gray-300 text-gray-600 items-center justify-center gap-2">
+                    <span className="text-2xl"><FaHeart /></span> <span className='text-sm'>300+ Likes </span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          })
+        }
+
+
+      </section>
     </article>
   )
 }
