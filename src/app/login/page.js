936799b9ba7ffc37f3ef71ac6388c6867f27498e
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-04-03 20:25:47 +0500

    LOGIN: created login form

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,24 @@
+import LoginFormComponent from '@/components/ui/auth/LoginFormComponent'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <article className="min-h-screen">
-       <section className="w-full">
-        <div className="container mx-auto">
-            <h1 className="text-4xl font-bold">Login</h1>
-            <p className="my-2 text-gray-700 dark:text-gray-200">Explore the latest articles and tutorials on modern web development technologies.</p>  
+      <section className="w-full">
+        <div className="md:w-[50%] w-[95%] mx-auto my-5 border-2 border-dotted border-white rounded-md p-4" >
+          <h1 className="text-4xl font-bold">Login</h1>
+          <p className="my-2 text-gray-700 dark:text-gray-200">Welcome back!, Enter your valid credientials to accees your account.</p>
+
+          <LoginFormComponent />
+
+          <div className='flex items-center justify-between gap-5 text-sm mt-5'>
+            <p className='dark:text-gray-200 text-gray-700'>Not have an account <Link href="/signup" className='text-blue-500 hover:underline'>Register</Link></p>
+            <Link href="/signup" className='text-blue-500 hover:underline'>Forget password</Link>
+          </div>
 
         </div>
-       </section>
+      </section>
     </article>
   )
 }
---
+++ src/components/ui/auth/LoginFormComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginFormComponent = () => {
+    return (
+        <form className=''>
+            <br />
+            <input type="email" name="email" autoComplete='off' placeholder="Email address" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+            <input type="password" name="password" autoComplete='off' placeholder="Password" className="my-3 dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit" />
+            <button className="submit w-auto p-3 border-1 border border-gray-500 text-white hover:bg-pink-600 bg-pink-700 text-center my-2 flex items-center justify-center gap-2 rounded-md active:border-2 active:border-dotted"> Login </button>
+        </form>
+    )
+}
+
+export default LoginFormComponent
