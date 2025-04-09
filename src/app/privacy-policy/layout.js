be68fb0e02879b67be024e2ba1caab1a29fab71a
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-04-09 20:28:44 +0500

    Enhance privacy policy layout metadata for improved SEO and user experience

--- src/app/privacy-policy/layout.js
+++ src/app/privacy-policy/layout.js
@@ -1,7 +1,12 @@
 
 export const metadata = {
     title:"Privacy-Policy || Ghazna.dev",
-    description:""
+    description: "Read the privacy policy of Ghazna.dev to understand how we handle your data and ensure your privacy.",
+    keywords: ["Privacy Policy", "Ghazna.dev", "Data Privacy", "User Data", "Privacy Practices"],
+    author: "Ghazna.dev",
+    robots: "index, follow",
+    viewport: "width=device-width, initial-scale=1.0",
+    charset: "UTF-8"
 }
 
 export default async function PrivacyPolicy({children}) {
