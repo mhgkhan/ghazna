de0435fa1d74b8d85b36dfc9bff202691bbf19b5
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-04-17 03:36:19 -0700

    Change link Typo Error

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -14,7 +14,7 @@ const page = () => {
 
           <div className='flex items-center justify-between gap-5 text-sm mt-5'>
             <p className='dark:text-gray-200 text-gray-700'>Not have an account <Link href="/signup" className='text-blue-500 hover:underline'>Register</Link></p>
-            <Link href="/signup" className='text-blue-500 hover:underline'>Forget password</Link>
+            <Link href="/forget_password" className='text-blue-500 hover:underline'>Forget password</Link>
           </div>
 
         </div>
