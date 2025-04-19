f3d1e22094ea8f1f8c08e6a39b366e7d19a7e90e
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-04-19 02:13:46 -0700

    Refactor profile layout and remove unused content from profile page

--- src/app/profile/layout.js
+++ src/app/profile/layout.js
@@ -9,7 +9,7 @@ export default async function ProfileRootLayout({ children }) {
                 <main className="min-h-screen bg-gray-100 dark:bg-gray-800 text-black dark:text-white">
                     <div className="flex items-start justify-between w-full h-full md:gap-3 gap-1 relative">
                         <ProfileSidebar />
-                        <article className="w-full h-full bg-gray-900 p-2 relative py-5 rounded-md">
+                        <article className="w-full h-full p-2 relative py-5 rounded-md">
                             {children}
                         </article>
                     </div>
