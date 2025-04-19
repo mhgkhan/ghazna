1c44421b9ca7531996cde7be65b41233c14751b8
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-04-19 03:10:27 -0700

    Refactor article component styling for improved layout and responsiveness

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -23,7 +23,7 @@ const page = async ({ params }) => {
     
 
     return (
-        <article className='min-h-screen'>
+        <article className='min-h-screen md:p-0 px-2' >
             <section role='banner' className="my-5">
                 <div className='container mx-auto sm:px-2 flex lg:flex-row flex-col items-start justify-center gap-10  '>
                     <div className='content lg:w-[70%] w-full sm:px-2'>
