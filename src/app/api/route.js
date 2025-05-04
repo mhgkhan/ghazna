10bde24861356194ec3414eb0da6300849ad485a
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-05-04 09:29:53 +0500

    Refactor POST request handler in API route and remove unused GET implementation

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,20 +1,11 @@
-import { NextResponse } from "next/server";
-
-import ConnectDB from "@/utils/db/ConnectDB";
-ConnectDB();
-export async function GET(request) {
 
 
-    // const platForm = await headers.get("");
-    const platform = request.headers.get("sec-ch-ua-platform")
-    const browser = request.headers.get("sec-ch-ua")?.split(";")[0];
+export async function POST(request) {
 
 
 
+    
 
-    try {
-        return NextResponse.json({ success: true, message: "welcoem to https://ghazna.vercel.app/" }, { status: 200, browser, platform });
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+
+
 }
