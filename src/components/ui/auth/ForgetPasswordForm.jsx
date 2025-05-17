b354ce36e062525cd5f09f7c3d34ff363030a36a
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-05-17 22:26:17 +0500

    enchance forget_password from button and response message dailog

--- src/components/ui/auth/ForgetPasswordForm.jsx
+++ src/components/ui/auth/ForgetPasswordForm.jsx
@@ -1,6 +1,10 @@
 "use client"
 
+import Loading from '@/components/Loading';
 import React, { useState } from 'react'
+import RespMessage from '../dailogs/RespMessage';
+import FormsButton from '../buttonsandlinks/FormsButton';
+import { FaPaperPlane } from 'react-icons/fa';
 
 const ForgetPasswordForm = () => {
 
@@ -74,9 +78,8 @@ const ForgetPasswordForm = () => {
     return (<form method="post" onSubmit={submitForm}>
 
         <input onChange={(e) => setEmail(e.target.value)} value={email} disabled={loading} type="email" name="email" autoComplete='off' placeholder="Email address" className="dark:text-white text-black font-bold dark:focus:bg-gray-700 focus:bg-gray-100 rounded-md p-2 w-full outline-none border border-1 dark:border-gray-300 border-gray-800 focus:border-2 focus:border-dotted bg-inherit disabled:bg-gray-300 disabled:text-gray-400" />
-        <button disabled={loading} type='submit' className="my-3 disabled:bg-gray-600 disabled:text-gray-800 submit w-auto p-3 border-1 border border-gray-500 text-white hover:bg-pink-600 bg-pink-700 text-center my-2 flex items-center justify-center gap-2 rounded-md active:border-2 active:border-dotted"> Submit  </button>
-
-        {message.length > 0 ? <div onClick={hideMessage} className={`my-5 py-5 px-3  rounded-md ${error ? "bg-red-600" : "bg-green-700"} text-white font-bold italic`}>{message}</div> : ""}
+        <FormsButton loading={loading} type={submitForm} text={"Submit"} icon={<FaPaperPlane />} />
+        {loading ? <Loading /> : message.length > 0 ? <RespMessage hide={hideMessage} isErr={error} message={message} /> : ""}
 
     </form>
 
