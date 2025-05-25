f1acef20648bca22b32ac0adfb830879ab770ad1
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-05-25 11:55:37 +0500

    Add BlogPostModel schema for managing blog posts with necessary fields and validation

---
+++ src/utils/models/BlogPostModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { defaultType } from "./schemaTypes";
+
+
+const blogPostSchema = new mongoose.Schema({
+    title: defaultType,
+    description: {
+        ...defaultType,
+        trim: true,
+    },
+    image: defaultType,
+    category: defaultType,
+    tags: defaultType,
+    content: defaultType,
+    slug: {
+        ...defaultType,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "draft", // or "saved"
+    },
+
+    isPublished: {
+        type: Boolean,
+        default: false,
+    },
+    publishedAt: {
+        type: Date,
+        default: null,
+    },
+
+}, {
+    timestamps: true,
+})
+
+
+const BlogPostModel = mongoose.models.blogposts || mongoose.model("blogposts", blogPostSchema);
+export default BlogPostModel;
+
