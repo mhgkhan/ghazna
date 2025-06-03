36ca96221b54638ffea637120459f51089505a3a
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-06-03 11:08:09 +0500

    Add BlogLikesModel schema for managing blog likes and dislikes

---
+++ src/utils/models/BlogLikesModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const blogLikeSchema = new mongoose.Schema({
+
+    liked: {
+        type: Boolean,
+        default: false
+    },
+
+    disliked: {
+        type: Boolean,
+        defult: false
+    },
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    },
+
+    blogId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "blogposts",
+    }
+
+
+
+}, { timestamps: true })
+
+
+const BlogReactModel = mongoose.models.bloglikes || mongoose.model('bloglikes', blogLikeSchema, 'bloglikes');
+export default BlogReactModel;
+// This code defines a Mongoose schema for a blog likes model, which includes fields for whether the post is liked or disliked, references to the user and blog post, and timestamps for when the like was created or updated.
+// The schema is then exported as a Mongoose model named 'bloglikes', allowing it to be used in other parts of the application for database operations related to blog likes.   
