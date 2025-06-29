d8f1560737c06c1367253c55c1e04a6ad3d61be5
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-06-29 10:30:17 +0500

    Add robots meta tag to prevent indexing by search engines

--- src/app/layout.js
+++ src/app/layout.js
@@ -84,7 +84,10 @@ export default async function RootLayout({ children }) {
     <html lang="en" className="scroll-m-20 scroll-smooth">
       <head>
         <meta name="apple-mobile-web-app-title" content="Ghazna Dev" />
+        <meta name="robots" content="noindex" />
         <GoogleSiteVerification contentVal={FreezeEnv.GoogleSiteVerificationContent} />
+
+
       </head>
       <body
         className={`${headingsFont.variable} ${rubikFont.variable} antialiased`}
