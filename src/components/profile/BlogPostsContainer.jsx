3708ecdefaeaa4575d53ba17b3b14d765bf95334
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-06-30 09:19:07 +0500

    Enhance BlogPostsContainer with sorting and layout toggle functionality; update FormsButton to accept additional classes for styling

--- src/components/profile/BlogPostsContainer.jsx
+++ src/components/profile/BlogPostsContainer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+import FormsButton from '../ui/buttonsandlinks/FormsButton'
+import { FaList, FaPlus, FaSortAmountDown, FaSortAmountUp } from 'react-icons/fa'
+import { useRouter } from 'next/navigation'
+import { BsGrid3X3Gap } from "react-icons/bs";
 
 const BlogPostsContainer = () => {
-    return (
-        <div className="blogs-container flex items-center justify-center gap-5 flex-wrap my-5">
-            <div className="blog-card rounded-md shadow-md shadow-gray-300 md:w-[30%] w-[290px] h-[300px]">
+    const router = useRouter();
 
+    const [sorted, setSorted] = useState(false);
+    const [isGrid, setIsGrid] = useState(true); // grid or list
+
+
+    return (
+        <div className="blogposts-section my-5">
+
+            <div className="blogs-filter flex items-center justify-between gap-5 border border-1 border-gra-300 rounded-md p-3 mb-5">
+                <FormsButton icon={<FaPlus />} text={"Create"} clickFun={() => router.push("/profile/blogs/create")} />
+                <div className="filter-buttons flex items-center justify-center gap-3">
+                    <FormsButton icon={sorted ? <FaSortAmountUp /> : <FaSortAmountDown />} clickFun={() => setSorted(!sorted)} classes={"text-xl"} />
+                    <FormsButton icon={isGrid ? <FaList /> : <BsGrid3X3Gap />} clickFun={() => setIsGrid(!isGrid)} classes={"text-xl"} />
+                </div>
+            </div>
+
+            <div className="blog-card rounded-md shadow-md shadow-gray-300 md:w-[30%] w-[290px] h-[300px]">
+                
             </div>
         </div>
 
--- src/components/ui/buttonsandlinks/FormsButton.jsx
+++ src/components/ui/buttonsandlinks/FormsButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const FormsButton = ({type, loading, text, icon, clickFun}) => {
+const FormsButton = ({type, loading, text, icon, clickFun, classes}) => {
     return (
-        <button onClick={clickFun} disabled={loading} type={type} className="submit w-auto p-3  border-gray-500 text-white bg-pink-600 dark:bg-white text-white dark:text-black font-bold text-center my-2 flex items-center justify-center gap-2 rounded-md border border-2 border-transparent active:border-white active:dark:border-black active:border-dotted">{icon?<span>{icon}</span>:""} {text}</button>
+        <button onClick={clickFun} disabled={loading} type={type} className={`submit w-auto p-3  border-gray-500 text-white bg-pink-600 dark:bg-white text-white dark:text-black font-bold text-center my-2 flex items-center justify-center gap-2 rounded-md border border-2 border-transparent active:border-white active:dark:border-black active:border-dotted ${classes}`}>{icon?<span>{icon}</span>:""} {text}</button>
     )
 }
 
