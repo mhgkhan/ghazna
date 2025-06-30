3dd8bf4c4e08cfee219cc3b6f899b1d317ae7dd5
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-06-30 10:11:24 +0500

    Update BlogPostsContainer layout for improved responsiveness

--- src/app/profile/blogs/page.js
+++ src/app/profile/blogs/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <article>
-      <div className="container mx-auto">
+      <div className="container mx-auto sm:px-1">
         <h2 className='md:text-3xl text-2xl font-bold my-5 text-pink-600 underline px-2 text-left'>Your Blogs   </h2>
         <p className='text-xl italic'>here you can edit, delete your blogs seriously.</p>
       
