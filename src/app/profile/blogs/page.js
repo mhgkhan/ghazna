a0de587ffca29bd6787bd658aaddf86c3e2f9815
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-06-30 10:17:11 +0500

    Update layout in page.js for improved responsiveness

--- src/app/profile/blogs/page.js
+++ src/app/profile/blogs/page.js
@@ -4,9 +4,9 @@ import React from 'react'
 const page = () => {
   return (
     <article>
-      <div className="container mx-auto sm:px-1">
-        <h2 className='md:text-3xl text-2xl font-bold my-5 text-pink-600 underline px-2 text-left'>Your Blogs   </h2>
-        <p className='text-xl italic'>here you can edit, delete your blogs seriously.</p>
+      <div className="container mx-auto ">
+        <h2 className='md:text-3xl text-2xl md:px-auto font-bold my-5 text-pink-600 underline px-2 text-left'>Your Blogs   </h2>
+        <p className='text-xl italic md:px-auto px-2'>here you can edit, delete your blogs seriously.</p>
       
       <BlogPostsContainer  />
       
