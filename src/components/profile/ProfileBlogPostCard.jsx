1dd01c99187e2363a2b233b39e7e94a77296fe32
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-07-07 22:05:41 +0500

    Remove unused icons from ProfileBlogPostCard component

--- src/components/profile/ProfileBlogPostCard.jsx
+++ src/components/profile/ProfileBlogPostCard.jsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import React from 'react'
 import FormsButton from '../ui/buttonsandlinks/FormsButton'
-import { FaBook, FaBookOpen, FaEdit, FaEyeSlash, FaList, FaPlus, FaSortAmountDown, FaSortAmountUp, FaTrash } from 'react-icons/fa'
+import { FaTrash, FaBookOpen, FaEdit, FaEyeSlash } from 'react-icons/fa'
 import { useRouter } from 'next/navigation'
 
 
@@ -15,7 +15,7 @@ const ProfileBlogPostCard = ({img,title,description, slug}) => {
             <Image src={img} width={300} alt='profile pic' height={370} className='rounded-lg w-full h-[200px] object-fill' />
             <div className="w-full px-2">
                 <h3 className='text-2xl font-bold mt-5'>{title.length>20?title.substring(0,20):title}</h3>
-                <p className=' dark:text-gray-200 text-gray-700 my-2'>{description.length>60?description.substring(0,60):description}</p>
+                <p className='dark:text-gray-200 text-gray-700 my-2'>{description.length>60?description.substring(0,60):description}</p>
             </div>
             <div className="action-buttons flex px-3 items-center justify-between gap-3 mb-0 border-t border-gray-300">
                 <FormsButton icon={<FaBookOpen />} clickFun={()=>router.push(`/blog/${slug}`)} />
