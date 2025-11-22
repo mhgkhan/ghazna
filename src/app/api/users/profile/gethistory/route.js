335000afa820a2c6540a56e59b22dc966a2b5c8c
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-11-22 11:51:35 +0500

    Implement user history retrieval API with authorization check

---
+++ src/app/api/users/profile/gethistory/route.js
@@ -0,0 +1,25 @@
+import { sendNormalResponse } from "@/utils/functions/sendResponses";
+import connectDB from "@/utils/db/connectDB";
+import { checkUserAuthorization } from "@/utils/functions/utilityFunctions";
+import HistoryModel from "@/utils/models/HistoryModel";
+
+
+
+connectDB();
+export async function GET(request) {
+
+    try {
+        // checking user authorization
+        const checkUserAuth = await checkUserAuthorization();
+        if (!checkUserAuth) {
+            return sendNormalResponse(false, checkUserAuth.status, checkUserAuth.message, null);
+        }
+
+        const history = await HistoryModel.find({ userId: checkUserAuth.user._id }).sort({ createdAt: -1 });
+
+        return sendNormalResponse(true, 200, "history is here", history);
+
+    } catch (error) {
+        return sendNormalResponse(false, 500, error.message, null)
+    }
+}
