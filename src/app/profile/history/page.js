2ff7967b81b9e0a0f2c8b9de3308fee2bec9d5f2
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-11-22 12:00:12 +0500

    Refactor history page to include fetchHistory function for improved data retrieval and error handling

--- src/app/profile/history/page.js
+++ src/app/profile/history/page.js
@@ -1,6 +1,46 @@
+import FreezeEnv from '@/config/EnvConfig';
 import React from 'react'
 
-const page = () => {
+
+const fetchHistory = async () => {
+  const obj = {};
+  try {
+
+    const request = await fetch(`${FreezeEnv.DOMAIN}api/users/profile/gethistory`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      }
+    });
+
+    const response = await request.json();
+    if (response.success) {
+      obj.error = false;
+      obj.message = response.message;
+      obj.data = response.data;
+      obj.success = true;
+    }
+    else {
+      obj.error = true;
+      obj.message = response.message;
+      obj.success = false;
+    }
+
+  } catch (error) {
+    obj.error = true;
+    obj.message = error.message;
+    // return obj;
+  } finally {
+    return obj;
+  }
+}
+
+const page = async () => {
+
+
+  const getHistory = await fetchHistory();
+
+
   return (
     <article>
       <div className="container mx-auto ">
@@ -13,14 +53,17 @@ const page = () => {
           <button className='px-4 py-2 bg-pink-600 text-white rounded-md hover:bg-pink-700 transition-all duration-300'>Clear all</button>
         </div>
 
-        <div className='my-2 history-all border border-1 border-dotted border-pink-500 rounded-md p-2 flex items-center justify-between gap-5'>
-          <div className="history-info flex items-center justify-start gap-2">
-            <span className='px-3 py-2 bg-pink-500 rounded-lg text-white font-bold'>1</span>
-            <p>this is your history text all the text</p>
-          </div>
-          <span className="italic">01/01/2025</span>
-        </div>
 
+        {
+          getHistory.success && getHistory.data.length > 0 ? <div className='my-2 history-all border border-1 border-dotted border-pink-500 rounded-md p-2 flex items-center justify-between gap-5'>
+            <div className="history-info flex items-center justify-start gap-2">
+              <span className='px-3 py-2 bg-pink-500 rounded-lg text-white font-bold'>1</span>
+              <p>this is your history text all the text</p>
+            </div>
+            <span className="italic">01/01/2025</span>
+          </div>
+            : <h3 className='text-2xl text-center text-pink-500 font-bold italic'>Nothing in your  History </h3>
+        }
 
 
       </div>
